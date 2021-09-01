d6316bf5f10f7491f8d0602648740adb6067a125
iliagromov <ilia.workproffile@gmail.com>
2021-09-01 14:54:20 +0300

    add template project

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+const gulp = require('gulp');
+
+// ===============================================
+// Task clear
+// ===============================================
+const clear = require('./gulp/gulp.task.clear');
+// ===============================================
+// Task cleanRemoveFiles
+// ===============================================
+const cleanRemoveFiles = require('./gulp/gulp.task.cleanRemoveFiles');
+
+
+// ===============================================
+// Tasks static
+// ===============================================
+const static = require('./gulp/gulp.task.static');
+
+
+// ===============================================
+// Tasks html
+// ===============================================
+const pug2html = require('./gulp/gulp.task.pug2html');
+
+// ===============================================
+// Tasks styles
+// ===============================================
+const styles = require('./gulp/gulp.task.styles');
+
+// ===============================================
+// Tasks grid
+// ===============================================
+const grid = require('./gulp/gulp.task.smartgrid');
+
+
+// ===============================================
+// Tasks scripts
+// ===============================================
+const script = require('./gulp/gulp.task.scripts');
+
+// ===============================================
+// Tasks
+// ===============================================
+
+gulp.task('build', gulp.parallel('htmlBuild', 'styles', 'scripts'));
+
+// ===============================================
+// Tasks watch
+// ===============================================
+const watch = require('./gulp/gulp.task.watch');
+const components = require('./gulp/gulp.create.components');

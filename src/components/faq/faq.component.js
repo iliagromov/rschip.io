0be548b408bb368926b9d5e56f0f1da4955632a2
iliagromov <ilia.workproffile@gmail.com>
2021-10-08 13:10:54 +0300

    add faq

--- src/components/faq/faq.component.js
+++ src/components/faq/faq.component.js
@@ -2,7 +2,12 @@ const faqItemDetails = document.querySelectorAll('.faqItemDetails');
 
 faqItemDetails && faqItemDetails.forEach((itemDetails)=>{
     itemDetails.addEventListener('click',(e)=>{
-        e.target.parentElement.classList.toggle('active');
-        e.target.classList.contains('faqItemDetails') &&  e.target.classList.toggle('active');
+        if(e.target.parentElement.classList.contains('faqItemDetails')){
+            e.target.parentElement.classList.toggle('active');
+        } else if(!e.target.parentElement.classList.contains('faqItemDetails')) {
+            e.target.parentElement.closest('.faqItemDetails').classList.toggle('active');
+        } else {
+            e.target.classList.contains('faqItemDetails') &&  e.target.classList.toggle('active');
+        }
     });
 })

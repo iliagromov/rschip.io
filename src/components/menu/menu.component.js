a689178f06ca0e26e8d1ed021533e4cb95038fa5
iliagromov <ilia.workproffile@gmail.com>
2021-10-09 14:20:46 +0300

    add menu

--- src/components/menu/menu.component.js
+++ src/components/menu/menu.component.js
@@ -1,5 +1,12 @@
 const btnHeaderNavBurgerMenu = document.querySelector('.headerNavBurgerMenu');
+const btnMenuClose = document.querySelector('.menuClose');
+
 const menu = document.querySelector('.menu');
-btnHeaderNavBurgerMenu.addEventListener('click',()=>{
-    menu.classList.add('.active');
+
+
+[btnHeaderNavBurgerMenu, btnMenuClose].forEach(function (element) {
+    element.addEventListener("click", function () {
+        menu.classList.toggle('active');
+        document.querySelector('body').classList.toggle('isMenuShow');
+    });
 });

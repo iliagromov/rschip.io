0acf8b183a9a6e87c3a47104b2720cace81201e3
iliagromov <ilia.workproffile@gmail.com>
2021-10-11 01:30:03 +0300

    add modal

--- src/libs/js/modal.js
+++ src/libs/js/modal.js
@@ -3,8 +3,6 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
-   /* Записываем в переменные массив элементов-кнопок и подложку.
-      Подложке зададим id, чтобы не влиять на другие элементы с классом overlay*/
    var modalButtons = document.querySelectorAll('.js-open-modal'),
        overlay      = document.querySelector('.js-overlay-modal'),
        closeButtons = document.querySelectorAll('.js-modal-close'),
@@ -12,31 +10,21 @@ document.addEventListener('DOMContentLoaded', function() {
        ;
 
 
-   /* Перебираем массив кнопок */
    modalButtons.forEach(function(item){
 
-      /* Назначаем каждой кнопке обработчик клика */
       item.addEventListener('click', function(e) {
 
-         /* Предотвращаем стандартное действие элемента. Так как кнопку разные
-            люди могут сделать по-разному. Кто-то сделает ссылку, кто-то кнопку.
-            Нужно подстраховаться. */
          e.preventDefault();
 
-         /* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
-            и будем искать модальное окно с таким же атрибутом. */
          var modalId = this.getAttribute('data-modal'),
              modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
 
-
-         /* После того как нашли нужное модальное окно, добавим классы
-            подложке и окну чтобы показать их. */
          modalElem.classList.add('active');
          overlay.classList.add('active');
          body.classList.add('isModalShow');
-      }); // end click
+      }); 
 
-   }); // end foreach
+   }); 
 
 
    closeButtons.forEach(function(item){
@@ -49,8 +37,7 @@ document.addEventListener('DOMContentLoaded', function() {
          body.classList.remove('isModalShow');
       });
 
-   }); // end foreach
-
+   }); 
 
     document.body.addEventListener('keyup', function (e) {
         var key = e.keyCode;
@@ -59,6 +46,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
             document.querySelector('.modal.active').classList.remove('active');
             document.querySelector('.overlay').classList.remove('active');
+            document.querySelector('body').classList.remove('isModalShow');
         };
     }, false);
 
@@ -66,9 +54,6 @@ document.addEventListener('DOMContentLoaded', function() {
     overlay.addEventListener('click', function() {
         document.querySelector('.modal.active').classList.remove('active');
         this.classList.remove('active');
+        document.querySelector('body').classList.remove('isModalShow');
     });
-
-
-
-
-}); // end ready
+}); 

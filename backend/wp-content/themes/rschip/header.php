a4e0290b8e0013683b56af51998df99cb7c092a8
iliagromov <ilia.workproffile@gmail.com>
2021-12-27 14:20:38 +0300

    поправил мету

--- backend/wp-content/themes/rschip/header.php
+++ backend/wp-content/themes/rschip/header.php
@@ -26,13 +26,16 @@ wp_reset_postdata(); ?>
     <meta charset="<?php bloginfo('charset') ?>">
     <base href="<?php echo get_template_directory_uri(); ?>/">
 
+    <title><?php echo wp_get_document_title(); ?></title>
+    <meta name="description" content="<?php bloginfo('description') ?>" />
+    
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="Ilia Gromov">
     <!-- <link rel="icon" type="image/png" href="assets/images/png/shared/favicon.png"> -->
     <!-- <link rel="icon" type="image/x-icon" href="assets/images/png/shared/favicon.ico"> -->
     <!-- <title>Chiptuning for you, +20% power and torque – RSchip chiptuning</title> -->
-    
+
     <link rel="apple-touch-icon" sizes="57x57" href="assets/images/png/shared/apple-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="60x60" href="assets/images/png/shared/apple-icon-60x60.png">
     <link rel="apple-touch-icon" sizes="72x72" href="assets/images/png/shared/apple-icon-72x72.png">
@@ -51,8 +54,7 @@ wp_reset_postdata(); ?>
     <meta name="msapplication-TileImage" content="assets/images/png/shared/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
     
-    <title><?php echo wp_get_document_title(); ?></title>
-    <meta name="description" content="<?php bloginfo('description') ?>" />
+    
     <!-- <meta name="description" content="Get extra 20% power and torque for your car. RSCHIP provides fuel saving and better MPG numbers. Try safe chiptuning today with free delivery for US and Canada."> -->
     <link rel="stylesheet" href="css/base-styles.css" type="text/css">
     <link rel="stylesheet" href="libs/css/swiper-bundle.min.css" type="text/css">
